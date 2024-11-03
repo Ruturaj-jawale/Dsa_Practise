b6d41f10520786e5ad2c15ed08c434409730d7e0
Ruturaj Prashant Jawale <163099074+Ruturaj-jawale@users.noreply.github.com>
2024-11-03 20:36:43 +0530

    Time: 1 ms (15.9%), Space: 41.1 MB (89.26%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!= goal.length()){
-            return false;
-        }
-        return (s+s).contains(goal);
+       BiFunction< String, String, Boolean > isRoatation = (str, target) ->
+       str.length() == target.length() && (str+str).contains(target);
+        return isRoatation.apply(s, goal);
     }
 }
