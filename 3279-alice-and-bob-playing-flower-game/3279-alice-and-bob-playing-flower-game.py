30d162171eb4a52ee5f7a757074a11bf900b5365
Ruturaj Prashant Jawale <163099074+Ruturaj-jawale@users.noreply.github.com>
2025-08-29 22:43:56 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.75%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def flowerGame(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        # odds_n = (n +1) // 2
+        # evens_n = n //2
+
+        # odds_m = (m + 1) //2
+        # evens_m = m//2
+
+        return (n * m) //2
