0bab08f19b617e5ad0534f5971c624bfcaa542c2
Ruturaj Prashant Jawale <163099074+Ruturaj-jawale@users.noreply.github.com>
2025-09-12 11:16:55 +0530

    Time: 327 ms (5.15%), Space: 18.1 MB (50.3%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        vs = set("aeiou")
-        v = sum( 1 for ch in s if ch in vs)
+        # vs = set("aeiou")
+        # v = sum( 1 for ch in s if ch in vs)
 
-        return v > 0
-        # if v == 0:
-        #     return False
-        # if v % 2 == 1:
-        #     return True
-        # return False
+        # return v > 0
+        return  sum( 1 for ch in s if ch in set("aeiou")) > 0
         
